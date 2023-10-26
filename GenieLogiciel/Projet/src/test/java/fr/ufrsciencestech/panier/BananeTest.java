4c30c72d338d5d52bacadc57457a4378feab202d
cristanemir <cristanemir@gmail.com>
2023-10-26 09:16:03 +0200

    ajout des classes de test vide

---
+++ GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/panier/BananeTest.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.ufrsciencestech.panier;
+
+import static junit.framework.Assert.*;
+import org.junit.*;
+
+/**
+ *
+ * @author cristanemir
+ */
+public class BananeTest {
+    Banane b;
+    
+    public BananeTest(){
+        b= new Banane();
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
---
+++ GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/panier/SimpleFruitTest.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.ufrsciencestech.panier;
+
+
+/**
+ *
+ * @author cristanemir
+ */
+public class SimpleFruitTest {
+
+
+}
