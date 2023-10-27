120168c58b170d221926e63ae24a267d59e883f5
salim1417 <115842913+salim1417@users.noreply.github.com>
2023-10-27 15:44:41 +0200

    Update Ananas.java

--- GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/panier/Ananas.java
+++ GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/panier/Ananas.java
@@ -17,23 +17,5 @@ public class Ananas extends SimpleFruit {
     public String toString() {
         return "Ananas du " + getOrigine() + " à " + getPrix() + " €";
     }
-     public static void main(String[] args) {
-        // Création d'un ananas pour les tests
-        Ananas ananas = new Ananas(0.5, "Brésil");
-
-        // Test fonctionnel : Vérification du prix
-        if (ananas.getPrix() == 0.5) {
-            System.out.println("Test du prix réussi");
-        } else {
-            System.out.println("Test du prix échoué");
-        }
-
-        // Test fonctionnel : Vérification de l'origine
-        if ("Brésil".equals(ananas.getOrigine())) {
-            System.out.println("Test de l'origine réussi");
-        } else {
-            System.out.println("Test de l'origine échoué");
-        }
-
-    }
+    
 }
