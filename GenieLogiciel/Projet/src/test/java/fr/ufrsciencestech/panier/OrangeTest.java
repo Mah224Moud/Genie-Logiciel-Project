3d20018ffbaa878226364e43d6a33039b68b3756
salim1417 <115842913+salim1417@users.noreply.github.com>
2023-10-27 16:39:01 +0200

    Update OrangeTest.java

--- GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/panier/OrangeTest.java
+++ GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/panier/OrangeTest.java
@@ -4,10 +4,48 @@
  */
 package fr.ufrsciencestech.panier;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  *
- * @author cristanemir
+ * @author salim1417
  */
 public class OrangeTest {
-    
-}
+    Orange o;
+
+    @Before
+    public void setUp() {
+        o = new Orange(0.9, "Etats Unies");
+    }
+
+    @Test
+    public void testGetPrix() {
+        assertEquals(0.9, o.getPrix(), 0.0);
+    }
+
+    @Test
+    public void testGetOrigine() {
+        assertEquals("Etats Unies", o.getOrigine());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Orange des Etats Unies à 0.9 €", o.toString());
+    }
+
+    @Test
+    public void testIsSeedless() {
+        assertFalse(o.isSeedless());
+    }
+
+    @Test
+    public void testEquals() {
+        Orange orange1 = new Orange(0.9, "Etats Unies");
+        Orange orange2 = new Orange(1.0, "France");
+
+        assertTrue(o.equals(orange1));
+        assertFalse(o.equals(orange2));
+    }
+
