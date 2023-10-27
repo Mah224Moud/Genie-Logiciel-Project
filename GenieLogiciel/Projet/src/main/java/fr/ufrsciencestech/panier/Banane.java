34d22314b4119c11470a35ba97a3c11594b165d2
salim1417 <115842913+salim1417@users.noreply.github.com>
2023-10-27 15:23:33 +0200

    Update Banane.java

--- GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/panier/Banane.java
+++ GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/panier/Banane.java
@@ -18,4 +18,22 @@ public class Banane extends SimpleFruit {
     public String toString() {
         return "Banane de " + this.getOrigine() + " à " + this.getPrix() + " €";
     }
+    public static void main(String[] args) {
+        // Création d'une banane pour les tests
+        Banane banane = new Banane();
+        
+        // Test fonctionnel : Vérification du prix
+        if (banane.getPrix() == 0.5) {
+            System.out.println("Test du prix réussi");
+        } else {
+            System.out.println("Test du prix échoué");
+        }
+
+        // Test fonctionnel : Vérification de l'origine
+        if ("Espagne".equals(banane.getOrigine())) {
+            System.out.println("Test de l'origine réussi");
+        } else {
+            System.out.println("Test de l'origine échoué");
+        }
+        
 }
