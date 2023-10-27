fad50c8900ee8dd87d3703d5c925d2848034bac2
habibatous <146194277+habibatous@users.noreply.github.com>
2023-10-28 01:18:27 +0200

    ajout test des methodes ViewGraphSwing

--- GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/view/VueGraphSwingTest.java
+++ GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/view/VueGraphSwingTest.java
@@ -3,11 +3,80 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package fr.ufrsciencestech.view;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 /**
  *
  * @author cristanemir
  */
 public class VueGraphSwingTest {
-    
-}
+
+    private VueGraphSwing vue;
+
+    @Before
+    public void setUp() {
+        vue = new VueGraphSwing();
+         }
+
+    @Test
+    public void testGetIncButton() {
+        JButton incButton = vue.getInc();
+        assertNotNull(incButton);
+        assertEquals("+", incButton.getText());
+    }
+
+    @Test
+    public void testGetDecButton() {
+        JButton decButton = vue.getDec();
+        assertNotNull(decButton);
+        assertEquals("-", decButton.getText());
+    }
+    @Test
+    public void testGetResetButton() {
+        JButton resetButton = vue.getReset();
+        assertNotNull(resetButton);
+        assertEquals("Vider le panier", resetButton.getText());
+    }
+
+    @Test
+    public void testGetBoycotteButton() {
+        JButton boycotteButton = vue.getBoycotte();
+        assertNotNull(boycotteButton);
+        assertEquals("Boycotter", boycotteButton.getText());
+    }
+
+    @Test
+    public void testGetCompteurLabel() {
+        JLabel compteurLabel = vue.getCompteur();
+        assertNotNull(compteurLabel);
+        assertEquals("0", compteurLabel.getText());
+    }
+
+    @Test
+    public void testGetShowTextArea() {
+        JTextArea showTextArea = vue.getShow();
+        assertNotNull(showTextArea);
+        assertEquals("", showTextArea.getText());
+    }
+
+    @Test
+    public void testGetPanierListComboBox() {
+        JComboBox panierListComboBox = vue.getPanierList();
+        assertNotNull(panierListComboBox);
+       
+    }
+
+    @Test
+    public void testGetCountryListComboBox() {
+        JComboBox countryListComboBox = vue.getCountryList();
+        assertNotNull(countryListComboBox);
+       
+    }
+
+  }
