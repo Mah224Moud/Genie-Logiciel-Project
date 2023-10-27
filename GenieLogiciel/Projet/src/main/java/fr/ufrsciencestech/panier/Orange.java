fff0833f7f919fe4a20966772342a3f5938518cf
salim1417 <115842913+salim1417@users.noreply.github.com>
2023-10-27 15:43:27 +0200

    Update Orange.java

--- GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/panier/Orange.java
+++ GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/panier/Orange.java
@@ -2,7 +2,7 @@ package fr.ufrsciencestech.panier;
 
 /**
  *
- * @author roudet
+ * @author salim1417
  */
 public class Orange extends SimpleFruit{
 	
@@ -18,5 +18,24 @@ public class Orange extends SimpleFruit{
     @Override
     public String toString(){
         return "Orange des " + getOrigine() + " à " + getPrix() + " €";
+    }
+
+    public static void main(String[] args) {
+        // Création d'une orange avec des paramètres pour les tests
+        Orange orange = new Orange(0.9, "Etats Unies");
+
+	// Test fonctionnel : Vérification du prix
+        if (orange.getPrix() == 0.9) {
+            System.out.println("Test du prix réussi");
+        } else {
+            System.out.println("Test du prix échoué");
+        }
+
+	// Test fonctionnel : Vérification de l'origine
+        if ("Etats Unies".equals(orange.getOrigine())) {
+            System.out.println("Test de l'origine réussi");
+        } else {
+            System.out.println("Test de l'origine échoué");
+        }
     }
 }
