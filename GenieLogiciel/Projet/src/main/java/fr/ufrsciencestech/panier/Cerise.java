f0af7899fc78498b976e076a771087d43c22643b
salim1417 <115842913+salim1417@users.noreply.github.com>
2023-11-02 17:09:19 +0100

    Update Cerise.java

--- GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/panier/Cerise.java
+++ GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/panier/Cerise.java
@@ -2,7 +2,7 @@ package fr.ufrsciencestech.panier;
 
 /**
  *
- * @author roudet
+ * @author salim1417
  */
 public class Cerise extends SimpleFruit{
     
@@ -19,5 +19,23 @@ public class Cerise extends SimpleFruit{
     @Override
     public String toString(){
         return "Cerise de " + this.getOrigine() + " à " + this.getPrix() + " €";
+    }
+    public static void main(String[] args) {
+        // Création d'une cerise  pour les tests
+        Cerise cerise = new Cerise(0.6, "Italie");
+
+	// Test fonctionnel : Vérification du prix
+        if (cerise.getPrix() == 0.6) {
+            System.out.println("Test du prix réussi");
+        } else {
+            System.out.println("Test du prix échoué");
+        }
+
+	 // Test fonctionnel : Vérification de l'origine
+        if ("Italie".equals(cerise.getOrigine())) {
+            System.out.println("Test de l'origine réussi");
+        } else {
+            System.out.println("Test de l'origine échoué");
+        }
     }
 }
