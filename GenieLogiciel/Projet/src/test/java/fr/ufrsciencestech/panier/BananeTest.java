478221794c0b35977c443e959b65e59e2193d91f
salim1417 <115842913+salim1417@users.noreply.github.com>
2023-11-02 17:14:53 +0100

    Update BananeTest.java

--- GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/panier/BananeTest.java
+++ GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/panier/BananeTest.java
@@ -4,25 +4,56 @@
  */
 package fr.ufrsciencestech.panier;
 
-import static junit.framework.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  *
- * @author cristanemir
+ * @author salim1417
  */
 public class BananeTest {
     Banane b;
     
-    public BananeTest(){
-        b= new Banane();
-    }
     
     @Before
     public void setUp() {
+        b = new Banane(0.5, "Espagne");
     }
     
     @After
     public void tearDown() {
+        b = null;
     }
+
+    @Test
+    public void testGetPrix() {
+        assertEquals(0.5, b.getPrix(), 0.0);
+    }
+
+    @Test
+    public void testGetOrigine() {
+        assertEquals("Espagne", b.getOrigine());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Banane de Espagne à 0.5 €", b.toString());
+    }
+
+     @Test
+    public void testIsSeedless() {
+        assertFalse(b.isSeedless());
+    }
+
+     @Test
+    public void testEquals() {
+        Banane banane1 = new Banane(0.5, "Espagne");
+        Banane banane2 = new Banane(0.7, "Costa Rica");
+
+        assertTrue(b.equals(banane1));
+        assertFalse(b.equals(banane2));
+    }
+
 }
