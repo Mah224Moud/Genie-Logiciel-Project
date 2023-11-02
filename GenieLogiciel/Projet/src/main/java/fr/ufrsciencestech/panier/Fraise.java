5cfd97bd9de3ae55904bdb66162b7b9b889945a3
salim1417 <115842913+salim1417@users.noreply.github.com>
2023-11-02 17:09:42 +0100

    Update Fraise.java

--- GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/panier/Fraise.java
+++ GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/panier/Fraise.java
@@ -6,7 +6,7 @@ package fr.ufrsciencestech.panier;
 
 /**
  *
- * @author mamoudou
+ * @author salim1417
  */
 public class Fraise extends SimpleFruit{
     public Fraise() {
@@ -20,5 +20,24 @@ public class Fraise extends SimpleFruit{
     @Override
     public String toString() {
         return "Fraise de " + this.getOrigine() + " à " + this.getPrix() + " €";
+    }
+
+    public static void main(String[] args) {
+        // Création d'une fraise pour les tests
+        Fraise fraise = new Fraise(0.35, "Hawai");
+
+        // Test fonctionnel : Vérification du prix
+        if (fraise.getPrix() == 0.35) {
+            System.out.println("Test du prix réussi");
+        } else {
+            System.out.println("Test du prix échoué");
+        }
+
+        // Test fonctionnel : Vérification de l'origine
+        if ("Hawai".equals(fraise.getOrigine())) {
+            System.out.println("Test de l'origine réussi");
+        } else {
+            System.out.println("Test de l'origine échoué");
+        }
     }
 }
