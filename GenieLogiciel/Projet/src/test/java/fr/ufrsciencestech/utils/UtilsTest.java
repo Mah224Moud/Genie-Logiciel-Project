2d811ceb614c4d28d4438a76dba3ad6865d9d2ac
DIALLO <safidjallo@gmail.com>
2023-11-03 17:01:38 +0100

    Ajout_Partie_Finale

--- GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/utils/UtilsTest.java
+++ GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/utils/UtilsTest.java
@@ -2,12 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package fr.ufrsciencestech.utils;
 
+package fr.ufrsciencestech.utils;
+import fr.ufrsciencestech.panier.Fruit;
+import org.junit.Test;
+import java.util.List;
 /**
  *
  * @author cristanemir
  */
 public class UtilsTest {
+    @Test
+    public void testListClassNamesInPackage() {
+        String packageName = "fr.ufrsciencestech.panier";
+        List<String> classNames = Utils.listClassNamesInPackage(packageName);
+        assert !classNames.isEmpty();
+    }
+
+    @Test
+    public void testCreateFruit() {
+        String className = "fr.ufrsciencestech.panier.Orange";
+        try {
+            Fruit fruit = Utils.createFruit(className);
+            assert fruit instanceof Fruit;
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCreateFruitWithParams() {
+        String className = "fr.ufrsciencestech.panier.Apple"; 
+        double prix = 2.0;
+        String origine = "France"; 
+        try {
+            Fruit fruit = Utils.createFruitWithParams(className, prix, origine);
+            assert fruit instanceof Fruit;
+            assert fruit.getPrix() == prix;
+            assert fruit.getOrigine().equals(origine);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
