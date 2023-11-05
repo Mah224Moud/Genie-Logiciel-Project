7d256a31ef85943cc35b074ea48fd7feeea50bc0
habibatous <146194277+habibatous@users.noreply.github.com>
2023-11-05 21:43:44 +0100

    vue SpecialBasketDialogue

--- GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/view/SpecialBasketDlgTest.java
+++ GenieLogiciel/Projet/src/test/java/fr/ufrsciencestech/view/SpecialBasketDlgTest.java
@@ -4,20 +4,67 @@
  */
 package fr.ufrsciencestech.view;
 
-import fr.ufrsciencestech.controller.Controleur;
-import fr.ufrsciencestech.utils.Panier;
+import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.Test;
+import javax.swing.JFrame;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author cristanemir
- */
+
+
+
+
 public class SpecialBasketDlgTest {
-    private  SpecialBasketDlg vue;
-    
+    private SpecialBasketDlg specialBasketDlg;
+    private JFrame parentFrame;
+    private List<String> fruits;
+
     @Before
     public void setUp() {
-        //Initialisation de la classe de test avant chaque test
-        //vue = new SpecialBasketDlg(null, false, null, new Panier(10), new Controleur());
+        parentFrame = new JFrame();
+        fruits = new ArrayList<>();
+        fruits.add("Fruit1");
+        fruits.add("Fruit2");
+        specialBasketDlg = new SpecialBasketDlg(parentFrame, true, fruits);
+    }
+
+    @Test
+    public void testGetSelectedFruitFromRadio() {
+        // Sélectionnez un bouton radio dans le test
+        specialBasketDlg.radioButtons.get(0).setSelected(true); // Sélectionnez le premier fruit
+
+        String expectedFruit = "Fruit1";
+        String actualFruit = specialBasketDlg.getSelectedFruitFromRadio();
+        assertEquals(expectedFruit, actualFruit);
+    }
+
+    @Test
+    public void testGetSelectedFruitsFromCheckbox() {
+        // Sélectionnez les cases à cocher dans le test
+        specialBasketDlg.checkboxButtons.get(0).setSelected(true); // Sélectionnez le premier fruit
+        specialBasketDlg.checkboxButtons.get(1).setSelected(true); // Sélectionnez le deuxième fruit
+
+        List<String> expectedFruits = new ArrayList<>();
+        expectedFruits.add("Fruit1");
+        expectedFruits.add("Fruit2");
+
+        List<String> actualFruits = specialBasketDlg.getSelectedFruitsFromCheckbox();
+        assertEquals(expectedFruits, actualFruits);
+    }
+
+    @Test
+    public void testGetChoice() {
+        String expectedChoice = "Jus de fruit simple";
+        String actualChoice = specialBasketDlg.getChoice();
+        assertEquals(expectedChoice, actualChoice);
+    }
+
+    @Test
+    public void testUpdatePicIcon() {
+    
     }
 }
+
+  
+
