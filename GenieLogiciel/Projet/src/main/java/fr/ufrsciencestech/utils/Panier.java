84f979fd5bde1f054a555b7e9eecf349fdaabc4e
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2023-11-07 23:58:14 +0100

    Added Utils package  documentation

--- GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/utils/Panier.java
+++ GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/utils/Panier.java
@@ -15,15 +15,30 @@ public class Panier {
     private int contenanceMax; // nb maximum d'oranges que peut contenir le panier
     private List<String> origines; // pour stocker les origines
 
-    // groupe 1
-    public Panier(int contenanceMax) { // initialise un panier vide ayant une certaine contenance maximale (precisee en
-        // parametre)
+    /**
+     * Constructor for the Panier class with specified maximum capacity.
+     * It creates a Panier object with an empty list of fruits and the given maximum
+     * capacity.
+     * It also initializes an empty list of origins.
+     *
+     * @param contenanceMax The maximum capacity of the Panier.
+     * @return An instance of the Panier class.
+     */
+    public Panier(int contenanceMax) {
         this.fruits = new ArrayList<Fruit>();
         this.contenanceMax = contenanceMax;
         this.origines = new ArrayList<>();
     }
 
-    public String toString() { // affichage de ce qui est contenu dans le panier : liste des fruits presents
+    /**
+     * This method returns a string representation of the Panier object.
+     * The string representation includes the list of fruits in the Panier.
+     * For each type of fruit in the Panier, it includes the name of the fruit and
+     * the number of that fruit in the Panier.
+     *
+     * @return String The string representation of the Panier object.
+     */
+    public String toString() {
         Map<String, Integer> fruitCount = new HashMap<>();
         StringBuilder res = new StringBuilder("Contenu du panier : \n\n");
 
@@ -45,7 +60,8 @@ public class Panier {
             } else {
                 res.append("\nLe panier contient " + this.getTaillePanier() + " fruit");
             }
-            res.append(" sur "+this.getContenanceMax()+" avec une valeur total de ").append(this.getPrix()).append("€");
+            res.append(" sur " + this.getContenanceMax() + " avec une valeur total de ").append(this.getPrix())
+                    .append("€");
 
         } else {
             res.append("\nLe panier ne contient aucun fruit il est complètement vide !!!");
@@ -54,38 +70,79 @@ public class Panier {
         return res.toString();
     }
 
-    // groupe 2
-    public ArrayList<Fruit> getFruits() { // accesseur du premier attribut
+    /**
+     * This method gets the list of fruits in the Panier.
+     *
+     * @return ArrayList<Fruit> The list of fruits in the Panier.
+     */
+    public ArrayList<Fruit> getFruits() {
         return this.fruits;
     }
 
-    public void setFruits(ArrayList<Fruit> fruits) { // modificateur du premier attribut
+    /**
+     * This method sets the list of fruits in the Panier.
+     *
+     * @param fruits The new list of fruits in the Panier.
+     * @return void
+     */
+    public void setFruits(ArrayList<Fruit> fruits) {
         this.fruits = fruits;
     }
 
-    public int getTaillePanier() { // accesseur retournant la taille allouee pour l'attibut fruits
+    /**
+     * This method gets the current size of the Panier.
+     * It returns the number of fruits in the Panier.
+     *
+     * @return int The current size of the Panier.
+     */
+    public int getTaillePanier() {
         return this.fruits.size();
     }
 
-    public int getContenanceMax() { // accesseur du second attribut
+    /**
+     * This method gets the maximum capacity of the Panier.
+     *
+     * @return int The maximum capacity of the Panier.
+     */
+    public int getContenanceMax() {
         return this.contenanceMax;
     }
 
-     public void setContenanceMax(int contenanceMax){  
-	this.contenanceMax = contenanceMax;
+    /**
+     * This method sets the maximum capacity of the Panier.
+     *
+     * @param contenanceMax The new maximum capacity of the Panier.
+     * @return void
+     */
+    public void setContenanceMax(int contenanceMax) {
+        this.contenanceMax = contenanceMax;
     }
-    // groupe 3
-    public Fruit getFruit(int i) { // accesseur retournant le fruit contenu dans le panier a l'emplacement n°i ou
-        // null s'il n'y a rien a cet emplacement
+
+    /**
+     * This method gets the fruit at the specified index in the Panier.
+     * If the index is out of range (index < 0 or index >= size of the Panier), it
+     * returns null.
+     *
+     * @param i The index of the fruit to get.
+     * @return Fruit The fruit at the specified index in the Panier, or null if the
+     *         index is out of range.
+     */
+    public Fruit getFruit(int i) {
         if (i >= 0 && i < fruits.size()) {
             return fruits.get(i);
         }
         return null;
     }
 
-    // groupe 4
-    public void ajout(Fruit o) throws PanierPleinException { // ajoute le fruit o a la fin du panier si celui-ci n'est
-        // pas plein
+    /**
+     * This method adds a fruit to the Panier.
+     * If the Panier is full, it throws a PanierPleinException.
+     *
+     * @param o The fruit to add to the Panier.
+     * @throws PanierPleinException If the Panier is full.
+     * @return void
+     */
+    public void ajout(Fruit o) throws PanierPleinException {
         if (fruits.size() < contenanceMax) {
             fruits.add(o);
         } else {
@@ -93,23 +150,46 @@ public class Panier {
         }
     }
 
-    public void setFruit(int i, Fruit f) { // modificateur du fruit contenu dans le panier a l'emplacement n°i par f
-        // (s'il y a bien deja un fruit a cet emplacement, ne rien faire sinon)
+    /**
+     * This method sets the fruit at the specified index in the Panier.
+     * If there is no fruit at the specified index, it does nothing.
+     *
+     * @param i The index of the fruit to set.
+     * @param f The new fruit to set at the specified index in the Panier.
+     * @return void
+     */
+    public void setFruit(int i, Fruit f) {
         if (i >= 0 && i < fruits.size() && fruits.get(i) != null) {
             fruits.set(i, f);
         }
     }
 
-    public boolean estVide() { // predicat indiquant que le panier est vide
+    /**
+     * This method checks if the Panier is empty.
+     *
+     * @return boolean Whether the Panier is empty.
+     */
+    public boolean estVide() {
         return this.fruits.isEmpty();
     }
 
-    public boolean estPlein() { // predicat indiquant que le panier est plein
+    /**
+     * This method checks if the Panier is full.
+     *
+     * @return boolean Whether the Panier is full.
+     */
+    public boolean estPlein() {
         return this.fruits.size() == contenanceMax;
     }
 
-    // groupe 5
-    public void retrait() throws PanierVideException { // retire le dernier fruit du panier si celui-ci n'est pas vide
+    /**
+     * This method removes the last fruit in the Panier.
+     * If the Panier is empty, it throws a PanierVideException.
+     *
+     * @throws PanierVideException If the Panier is empty.
+     * @return void
+     */
+    public void retrait() throws PanierVideException {
         if (!fruits.isEmpty()) {
             fruits.remove(fruits.size() - 1);
         } else {
@@ -117,24 +197,35 @@ public class Panier {
         }
     }
 
-    public double getPrix() { // calcule le prix du panier par addition des prix de tous les fruits contenus
-        // dedans
+    /**
+     * This method calculates the total price of the Panier.
+     * It adds up the prices of all the fruits in the Panier.
+     *
+     * @return double The total price of the Panier.
+     */
+    public double getPrix() {
         double prixTotal = 0.0;
         for (Fruit fruit : this.fruits) {
             prixTotal += fruit.getPrix();
         }
 
-        // Utiliser DecimalFormat avec la locale "en-US" pour formater le résultat
         DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
         DecimalFormat df = new DecimalFormat("0.00", symbols);
         String formattedPrix = df.format(prixTotal);
 
-        // Convertir la chaîne formatée en double
         return Double.parseDouble(formattedPrix);
     }
 
-    // groupe 7
-    public void boycotteOrigine(String origine) { // supprime du panier tous les fruits provenant du pays origine
+    /**
+     * This method removes all the fruits in the Panier that come from the specified
+     * origin.
+     * It iterates over the list of fruits in the Panier, and if a fruit comes from
+     * the specified origin, it removes that fruit.
+     *
+     * @param origine The origin of the fruits to remove from the Panier.
+     * @return void
+     */
+    public void boycotteOrigine(String origine) {
         for (int i = fruits.size() - 1; i >= 0; --i) {
             if (fruits.get(i).getOrigine().equals(origine)) {
                 fruits.remove(i);
@@ -142,10 +233,16 @@ public class Panier {
         }
     }
 
-    // groupe 8    
+    /**
+     * This method checks if the Panier is equal to another object.
+     * It checks if the other object is not null, is an instance of the Panier
+     * class, and contains exactly the same fruits as the Panier.
+     *
+     * @param o The object to be compared with the Panier.
+     * @return boolean Whether the Panier is equal to the other object.
+     */
     @Override
-    public boolean equals(Object o) {/// predicat pour tester si 2 paniers sont equivalents : s'ils contiennent
-        /// exactement les memes fruits
+    public boolean equals(Object o) {
         if (o != null && o instanceof Panier) {
             Panier or = (Panier) o;
             if (getTaillePanier() == or.getTaillePanier()) {
@@ -162,6 +259,18 @@ public class Panier {
         return false;
     }
 
+    /**
+     * This method removes all the fruits in the Panier and clears the list of
+     * origins.
+     * If the Panier is already empty, it returns a message indicating that the
+     * Panier is already empty.
+     * Otherwise, it clears the list of fruits and the list of origins in the
+     * Panier, and returns a message indicating that the Panier has been completely
+     * emptied.
+     *
+     * @return String A message indicating whether the Panier was already empty or
+     *         has been completely emptied.
+     */
     public String clearAll() {
         if (this.estVide()) {
             return "Le panier est dèjà vide !!!";
@@ -172,6 +281,13 @@ public class Panier {
         }
     }
 
+    /**
+     * This method retrieves the list of origins of the fruits in the Basket.
+     * It creates a new list of origins, adds the origin of each fruit in the Basket
+     * to the list, and then returns a new list without duplicates.
+     *
+     * @return List<String> The list of unique origins of the fruits in the Basket.
+     */
     public List<String> getOriginesSansDoublons() {
         List<String> originesDoublons = new ArrayList<>();
 
@@ -184,6 +300,13 @@ public class Panier {
         return new ArrayList<>(originesSansDoublons);
     }
 
+    /**
+     * This method gets the list of unique origins of the fruits in the Panier.
+     * It assigns the list of unique origins to the list of origins in the Panier,
+     * and then returns the list of origins in the Panier.
+     *
+     * @return List<String> The list of unique origins of the fruits in the Panier.
+     */
     public List<String> getOrigines() {
         return this.origines = getOriginesSansDoublons();
     }
--- GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/utils/Utils.java
+++ GenieLogiciel/Projet/src/main/java/fr/ufrsciencestech/utils/Utils.java
@@ -14,6 +14,16 @@ import java.util.List;
  */
 public class Utils {
 
+    /**
+     * This method lists the names of all the classes in the specified package.
+     * It uses the ClassLoader to get the path of the package, and then lists all
+     * the files in that path that end with ".class".
+     *
+     * @param packageName The name of the package.
+     * @return List<String> The names of all the classes in the specified package.
+     * @throws ClassNotFoundException If the specified package cannot be located.
+     * @throws IOException            If an I/O error occurs.
+     */
     public static List<String> listClassNamesInPackage(String packageName) {
         List<String> classNames = new ArrayList<>();
         try {
@@ -43,7 +53,24 @@ public class Utils {
         return classNames;
     }
 
-    public static Fruit createFruit(String className) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    /**
+     * This method creates an instance of a class that extends Fruit.
+     * It throws an IllegalArgumentException if the specified class does not extend
+     * Fruit.
+     *
+     * @param className The name of the class that extends Fruit.
+     * @return Fruit An instance of the specified class.
+     * @throws ClassNotFoundException If the specified class cannot be located.
+     * @throws InstantiationException If this Class represents an abstract class, an
+     *                                interface, an array class, a primitive type,
+     *                                or void; or if the class has no nullary
+     *                                constructor; or if the instantiation fails for
+     *                                some other reason.
+     * @throws IllegalAccessException If the class or its nullary constructor is not
+     *                                accessible.
+     */
+    public static Fruit createFruit(String className)
+            throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Class<?> fruitClass = Class.forName(className);
         if (Fruit.class.isAssignableFrom(fruitClass)) {
             Fruit instance = (Fruit) fruitClass.newInstance();
@@ -53,13 +80,34 @@ public class Utils {
         }
     }
 
-    public static Fruit createFruitWithParams(String className, double prix, String origine) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    /**
+     * This method creates an instance of a class that extends Fruit, and sets the
+     * price and origin of the fruit.
+     * It throws an IllegalArgumentException if the specified class does not extend
+     * Fruit.
+     *
+     * @param className The name of the class that extends Fruit.
+     * @param prix      The price of the fruit.
+     * @param origine   The origin of the fruit.
+     * @return Fruit An instance of the specified class, with the specified price
+     *         and origin.
+     * @throws ClassNotFoundException If the specified class cannot be located.
+     * @throws InstantiationException If this Class represents an abstract class, an
+     *                                interface, an array class, a primitive type,
+     *                                or void; or if the class has no nullary
+     *                                constructor; or if the instantiation fails for
+     *                                some other reason.
+     * @throws IllegalAccessException If the class or its nullary constructor is not
+     *                                accessible.
+     */
+    public static Fruit createFruitWithParams(String className, double prix, String origine)
+            throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Class<?> fruitClass = Class.forName(className);
         if (Fruit.class.isAssignableFrom(fruitClass)) {
             Fruit instance = (Fruit) fruitClass.newInstance();
             instance.setPrix(prix);
             instance.setOrigine(origine);
-            
+
             return instance;
         } else {
             throw new IllegalArgumentException("La classe spécifiée ne doit pas être une sous-classe de Fruit.");
